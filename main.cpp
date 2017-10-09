303d794a926b3ef7090c1ac4938374e670ba849e
valeriabv <lerina10@mail.ru>
2017-10-09 23:01:20 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+double a,b;
+
+cout<<"Enter x: ";
+cin>>a;
+cout<<"Enter y: ";
+cin>>b;
+
+    cout << "x + y = " << a+b << endl;
+    cout << "x - y = " << a-b << endl;
+    cout << "x / y = " << a/b << endl;
+    cout << "x * y = " << a*b << endl;
+}
